7d19c23744b8fc85ba157e33ccd1621028477630
T-mura-mura <takumu.kawamura@gmail.com>
2020-01-01 03:54:38 +0900

    clock.py、'interval'で成功したので、
    今度は'cron'でいけるか。

--- clock.py
+++ clock.py
@@ -1,17 +1,13 @@
-# from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.schedulers.blocking import BlockingScheduler
 import subprocess
-# from hello.management.commands.scraping_books import Command
 
-# sched = BackgroundScheduler()
 sched = BlockingScheduler()
 
-# @sched.scheduled_job('cron', hour=6, minute=35)
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('cron', hour=4, minute=30)
+# @sched.scheduled_job('interval', minutes=3)
 def scheduled_job():
     cmd = "python manage.py scraping_books"
     subprocess.call(cmd.split())
-    # Command.handle
 
 sched.start()
 
