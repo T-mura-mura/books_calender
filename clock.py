6cbb7b97255c06c746f0dcadcd3dad942a8a8b62
T-mura-mura <takumu.kawamura@gmail.com>
2020-01-01 03:11:01 +0900

    定期実行トライ。

--- clock.py
+++ clock.py
@@ -1,12 +1,15 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-import subprocess
+# import subprocess
+from hello.management.commands.scraping_books import Command
 
 sched = BackgroundScheduler()
 
-@sched.scheduled_job('cron', hour=6, minute=35)
+# @sched.scheduled_job('cron', hour=6, minute=35)
+@sched.scheduled_job('interval', minutes=5)
 def scheduled_job():
-    cmd = "python manage.py scraping_books"
-    subprocess.call(cmd.split())
+    # cmd = "python manage.py scraping_books"
+    # subprocess.call(cmd.split())
+    Command.handle
 
 sched.start()
 
