3e24fa255744b7ae4dddb7df068bd8754551bd3c
T-mura-mura <takumu.kawamura@gmail.com>
2020-01-02 07:31:18 +0900

    clockとwebプロセスがスリープ
    しないように、定期的にWebページにcurlでgetメソッドを出すのと、8時にメール出すようにした。

--- clock.py
+++ clock.py
@@ -3,11 +3,15 @@ import subprocess
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=4, minute=30)
+@sched.scheduled_job('interval', minutes=20)
+def timed_job():
+  cmd = "curl https://books-date.herokuapp.com/"
+  subprocess.call(cmd.split())
 
+@sched.scheduled_job('cron', hour=8)
 def scheduled_job():
-    cmd = "python manage.py scraping_books"
-    subprocess.call(cmd.split())
+  cmd = "python manage.py scraping_books"
+  subprocess.call(cmd.split())
 
 sched.start()
 
